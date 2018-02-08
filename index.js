f6c5c9681c4260e1ce029d5f87fb007851d18250
hanihoumani93 <hani-houmani93@hotmail.com>
2018-02-08 11:53:41 +0200

    server-setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err)=>{
+    if(err){
+        console.log('Could Not Connecte to DB ', err);
+    }else{
+        console.log('Connected to DB '+config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+app.listen(8080, ()=>{
+    console.log('Server is running on port 8080');
+});
